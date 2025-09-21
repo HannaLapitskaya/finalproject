93f7c1cb6dc22587872486f79df21fb82000630b
HannaLapitskaya <anna.lapitskaya6@gmail.com>
2025-09-22 01:12:58 +0300

    add BaseTest for API tests with setUp and tearDown

---
+++ src/test/java/by/megatop/api/BaseTest.java
@@ -0,0 +1,18 @@
+package by.megatop.api;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class BaseTest {
+    protected LoginService service;
+
+    @BeforeEach
+    void setUp() {
+        service =  new LoginService();
+    }
+
+    @AfterEach
+    void  tearDown() {
+        service = null;
+    }
+}
