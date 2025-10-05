4236fd010faea31de3289fb49d14dbf7c46b7597
HannaLapitskaya <anna.lapitskaya6@gmail.com>
2025-10-05 22:29:11 +0300

    refactor code

--- src/test/java/by/megatop/ui/BaseTest.java
+++ src/test/java/by/megatop/ui/BaseTest.java
@@ -21,6 +21,7 @@ public class BaseTest {
         basePage.openSite();
         basePage.clickCityByLocation();
         basePage.clickAcceptCookiesButton();
+        WaitUtils.waitForPageToLoad();
         basePage.clickWomanCategory();
     }
 
