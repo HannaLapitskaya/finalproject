a842c0b5ab35629ccfd7d53c331833648fae8363
HannaLapitskaya <anna.lapitskaya6@gmail.com>
2025-10-05 22:15:58 +0300

    add additional methods and tests

--- src/main/java/by/megatop/ui/pages/HomePage.java
+++ src/main/java/by/megatop/ui/pages/HomePage.java
@@ -8,6 +8,11 @@ public class HomePage {
 
     private static final Logger logger = LogManager.getLogger();
     private static final String SPAN_CONTACT_PHONE = "//span[@class='my-auto']";
+    private static final String HEADER_WOMEN_TREND_ZONE = "(//a[@class='categories__title font-weight-medium'])[1]";
+    private static final String HEADER_WOMEN_SHOES_CATEGORY = "(//a[@class='categories__title font-weight-medium'])[2]";
+    private static final String HEADER_ACCESSORIES_CATEGORY = "(//a[@class='categories__title font-weight-medium'])[3]";
+    private static final String HEADER_HABERDASHERY_CATEGORY= "(//a[@class='categories__title font-weight-medium'])[4]";
+    private static final String HEADER_DESIGN_IN_KOREA_CATEGORY = "(//a[@class='categories__title font-weight-medium'])[5]";
 
     public HomePage() {
 
@@ -16,5 +21,25 @@ public class HomePage {
     public String getContactPhoneText() {
         logger.debug("Getting contact phone text");
         return DriverManager.getTextFromElement(SPAN_CONTACT_PHONE);
+    }
+
+    public String getHeaderWomenTrendZoneCategoryText() {
+        return DriverManager.getTextFromElement(HEADER_WOMEN_TREND_ZONE);
+    }
+
+    public String getHeaderWomenShoesCategoryText() {
+        return DriverManager.getTextFromElement(HEADER_WOMEN_SHOES_CATEGORY);
+    }
+
+    public String getHeaderAccessoriesCategoryText() {
+        return DriverManager.getTextFromElement(HEADER_ACCESSORIES_CATEGORY);
+    }
+
+    public String getHeaderHaberdasheryCategoryText() {
+        return DriverManager.getTextFromElement(HEADER_HABERDASHERY_CATEGORY);
+    }
+
+    public String getHeaderDesignInKoreaCategoryText() {
+        return DriverManager.getTextFromElementWhenVisible(HEADER_DESIGN_IN_KOREA_CATEGORY);
     }
 }
--- src/test/java/by/megatop/ui/HomeTest.java
+++ src/test/java/by/megatop/ui/HomeTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Home page functionality tests")
+@DisplayName("Home page UI tests")
 public class HomeTest extends BaseTest {
 
     private static final Logger logger = LogManager.getLogger();
@@ -22,6 +22,71 @@ public class HomeTest extends BaseTest {
         String actualResult = homePage.getContactPhoneText();
 
         Assertions.assertEquals(expectedResult, actualResult);
-        logger.info("Test 'The presence of a contact phone number' passed successfully");
+        logger.info("Contact phone number test passed successfully");
+    }
+
+    @Test
+    @DisplayName("Design in Korea category header should be displayed")
+    public void designInKoreaCategoryHeaderShouldBeDisplayed() {
+        logger.debug("Starting Design in Korea category header verification test");
+        HomePage homePage = new HomePage();
+
+        String expectedResult = "Design in Korea";
+        String actualResult = homePage.getHeaderDesignInKoreaCategoryText();
+
+        Assertions.assertEquals(expectedResult, actualResult);
+        logger.info("Design in Korea category header test passed successfully");
+    }
+
+    @Test
+    @DisplayName("Trend Zone category header should be displayed")
+    public void trendZoneCategoryHeaderShouldBeDisplayed() {
+        logger.debug("Starting Trend Zone category header verification test");
+        HomePage homePage = new HomePage();
+
+        String expectedResult = "Зона трендов";
+        String actualResult = homePage.getHeaderWomenTrendZoneCategoryText();
+
+        Assertions.assertEquals(expectedResult, actualResult);
+        logger.info("Trend Zone category header test passed successfully");
+    }
+
+    @Test
+    @DisplayName("Accessories category header should be displayed")
+    public void accessoriesCategoryHeaderShouldBeDisplayed() {
+        logger.debug("Starting Accessories category header verification test");
+        HomePage homePage = new HomePage();
+
+        String expectedResult = "Аксессуары";
+        String actualResult = homePage.getHeaderAccessoriesCategoryText();
+
+        Assertions.assertEquals(expectedResult, actualResult);
+        logger.info("Accessories category header test passed successfully");
+    }
+
+    @Test
+    @DisplayName("Women Shoes category header should be displayed")
+    public void womenShoesCategoryHeaderShouldBeDisplayed() {
+        logger.debug("Starting Women Shoes category header verification test");
+        HomePage homePage = new HomePage();
+
+        String expectedResult = "Обувь";
+        String actualResult = homePage.getHeaderWomenShoesCategoryText();
+
+        Assertions.assertEquals(expectedResult, actualResult);
+        logger.info("Women Shoes category header test passed successfully");
+    }
+
+    @Test
+    @DisplayName("Haberdashery category header should be displayed")
+    public void haberdasheryCategoryHeaderShouldBeDisplayed() {
+        logger.debug("Starting Haberdashery category header verification test");
+        HomePage homePage = new HomePage();
+
+        String expectedResult = "Галантерея";
+        String actualResult = homePage.getHeaderHaberdasheryCategoryText();
+
+        Assertions.assertEquals(expectedResult, actualResult);
+        logger.info("Haberdashery category header test passed successfully");
     }
 }
