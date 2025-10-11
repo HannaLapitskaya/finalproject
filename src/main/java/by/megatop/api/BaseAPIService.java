b417e9207a9f47a6259db63a15e79c2550c4860d
HannaLapitskaya <anna.lapitskaya6@gmail.com>
2025-10-11 22:54:11 +0300

    add BaseAPIService

---
+++ src/main/java/by/megatop/api/BaseAPIService.java
@@ -0,0 +1,40 @@
+package by.megatop.api;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public abstract class BaseAPIService {
+    private Response response;
+
+    public void doPost(String body, String URL) {
+        response = given()
+                .headers(getHeaders())
+                .body(body)
+                .when().post(URL);
+    }
+
+    public void doGet(String URL, Map<String, String> params) {
+        response = given()
+                .headers(getHeaders())
+                .queryParams(params)
+                .when().get(URL);
+    }
+
+    public int getStatusCode() {
+        return response.getStatusCode();
+    }
+
+    public String getBody() {
+        return response.getBody().asPrettyString();
+    }
+
+    public JsonPath getJsonPath() {
+        return new JsonPath(getBody());
+    }
+
+    protected abstract Map<String, String> getHeaders();
+}
